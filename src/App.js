7fe1f11d0804e5a64e063a45dbcdd5b680285476
Krae Wind <kraewind@gmail.com>
2021-07-10 16:19:22 -0400

    commit tree 1ae003a0fe3bd2b5e2b0df83aa8e0d168506cc4e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = {
+            timerIDs: []
+        }
+      }
+  componentDidMount(){
+      return fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }  
+  
+  render() {
+    console.log(this.state.timerIDs);
+    return (
+      <div className="App">
+       
+
+      </div>
+    );
+  }
+
+}
+
+export default App;
